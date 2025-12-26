f77969023d72182f7cb7069d714fed870cfab09b
Sanmimimi <3864620423@qq.com>
2025-12-26 22:47:58 +0800

    Update sidebars.ts

--- sidebars.ts
+++ sidebars.ts
@@ -3,21 +3,15 @@ import type { SidebarsConfig } from '@docusaurus/plugin-content-docs'
 export default {
   docs: [
     {
-      label: 'Docusaurus 主题魔改',
+      label: '说说',
       type: 'category',
       link: {
         type: 'doc',
-        id: 'docusaurus/docusaurus-guides',
+        id: 'hi/hi-nihao',
       },
       items: [
-        'docusaurus/docusaurus-config',
-        'docusaurus/docusaurus-style',
-        'docusaurus/docusaurus-component',
-        'docusaurus/docusaurus-plugin',
-        'docusaurus/docusaurus-search',
-        'docusaurus/docusaurus-comment',
-        'docusaurus/docusaurus-deploy',
-      ],
+        'hi/hi-comment',
+      ],                   
     },
   ],
 } satisfies SidebarsConfig
